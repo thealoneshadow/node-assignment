f311089273686e158c653a4fa7122a898f7a02aa
Divyanshu Goyal <divyanshu.goyal@cybergindia.local>
2023-03-03 18:03:17 +0530

    Updated Node-Assignemnt

--- src/models/UserOTPVerification.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userOTPSchema = new mongoose.Schema({
-    userId: String,
-    otp: String,
-},{timestamps: true});
-
-
-module.exports = mongoose.model("UserOTPVerification", userOTPSchema);
